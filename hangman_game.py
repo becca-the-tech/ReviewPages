52246abb1de984b67240bea21f883915d6e088a2
Becca <becca.the.tech@gmail.com>
2021-02-14 16:13:00 -0500

    Hangman game finished

--- hangman_game.py
+++ hangman_game.py
@@ -1,33 +1,103 @@
 import random
 
-word_list = ["aardvark", "baboon", "camel"]
-
-chosen_word = random.choice(word_list)
-
-#guess= input("Guess a letter: ").lower()
-
-display = []
-
-for letter in chosen_word:
-    display.append('_')
-
-print("Chosen word was: " + chosen_word)
-print(display)
+stages = [
+    '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+'''
+]
 
 end_of_game = False
+word_list = ["aardvark", "baboon", "camel"]
+chosen_word = random.choice(word_list)
+word_length = len(chosen_word)
 
-#either while option below works properly
-#while '_' in display:
-#while display.count('_') >= 1:
+#lives until game is over
+lives = 6
+
+#Testing code
+#print(f'Pssst, the solution is {chosen_word}.')
+
+#Create blanks
+display = []
+for _ in range(word_length):
+    display += "_"
+
 while not end_of_game:
     guess = input("Guess a letter: ").lower()
-    counter = 0
 
-    for letter in chosen_word:
+    #Check guessed letter
+    for position in range(word_length):
+        letter = chosen_word[position]
         if letter == guess:
-            display[counter] = guess
-        counter += 1
+            display[position] = letter
 
-    print(display)
-    if not '_' in display:
+    if not guess in chosen_word:
+        lives -= 1
+
+    if lives == 0:
+        print(stages[0])
+        print("You lose! :(")
+        break
+
+    #Join all the elements in the list and turn it into a String.
+    print(f"{' '.join(display)}")
+
+    #Check if user has got all letters.
+    if "_" not in display:
         end_of_game = True
+        print("You win! :)")
+
+    print(stages[lives])

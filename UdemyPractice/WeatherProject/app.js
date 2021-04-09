927317132b1d84af247b5b9731229c44cd00525b
Becca <becca.the.tech@gmail.com>
2021-04-09 11:17:57 -0400

    Weather app now has a form for input and uses that with Post requests to change which city is displayed.

--- UdemyPractice/WeatherProject/app.js
+++ UdemyPractice/WeatherProject/app.js
@@ -1,17 +1,54 @@
 const express = require("express");
 const https = require("https");
+const bodyParser = require("body-parser");
 
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.get("/", function(req, res) {
-
-    const url = "https://api.openweathermap.org/data/2.5/weather?q=Tampa&appid=fb490fe5c4bf14e08a4881b51a327b5c";
-    https.get(url, function(response) {
-        console.log(response.statusCode);
-    });
-
-    res.send("Server is up and running on port 3000.");
+    res.sendFile(__dirname + "/index.html");
 });
+
+app.post("/", function(req, res) {
+    const query = req.body.cityName;
+    const apiKey = "fb490fe5c4bf14e08a4881b51a327b5c";
+    const unit = "imperial";
+
+    const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&appid=" + apiKey + "&units=" + unit;
+    https.get(url, function(response) {
+        //console.log(response.statusCode);
+
+        response.on("data", function(data) {
+            const weatherData = JSON.parse(data);
+            const temp = weatherData.main.temp;
+
+            const weatherDescription = weatherData.weather[0].description;
+
+            //multiple res.write()'s can be used, unlike res.send()
+            const header = "<h1>Weather in " + query + "</h1>";
+            const displayTemp = "<p>The  temperature in " + query + " is " + temp + " degrees Fahrenheit. </p>";
+            const displayDescription = "<p>The weather in " + query + " is currently " + weatherDescription + ".<p>";
+
+            //when trying to displayIcon above header, it kept breaking the processing of the html so keep images below h1
+            const icon = weatherData.weather[0].icon;
+            const iconLocation = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
+            const displayIcon = "<img src=\"" + iconLocation + "\" alt=\"weather-img\" style='background: blue;'>";
+
+            //            res.write("<img src='" + iconLocation + "' alt='weather-img'>");
+            res.write(header);
+            res.write(displayIcon);
+            res.write(displayTemp);
+            res.write(displayDescription);
+
+            res.send();
+        });
+    });
+});
+//There can only be one res.send() per file, more than one causes the app to crash with the error code: "ERR_HTTP_HEADERS_SENT"
+//    res.send("Server is up and running on port 3000.");
+
+
 
 
 

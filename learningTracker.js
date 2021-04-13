f739c29d151e36b26c2042b29364f9f528b38a6e
Becca <becca.the.tech@gmail.com>
2021-04-13 17:08:53 -0400

    Updates styling of learningTracker, adds image as the header with nav and img-text showing with opacity.. also changes sort-arrow from right-side to downward... as for bookList, adds some styling to the form (background color and spacing).

--- learningTracker.js
+++ learningTracker.js
@@ -4,5 +4,7 @@ let containers = document.getElementsByClassName("container");
 for (let i = 0; i < downButtons.length; i++) {
     downButtons[i].addEventListener("click", function() {
         containers[i].classList.toggle("hidden");
+        downButtons[i].classList.toggle("fa-arrow-circle-right");
+        downButtons[i].classList.toggle("fa-arrow-circle-down");
     });
 }

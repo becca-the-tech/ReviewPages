91c1d608b9072324c7c7e3edd1eb5d40eceba442
Becca <becca.the.tech@gmail.com>
2021-05-16 23:47:54 -0400

    Changes wording on portfolio page, adds the studyFullStack bootstrap page

---
+++ switchPractice.js
@@ -0,0 +1,29 @@
+/* --------------- Challenge 1 --------------*/
+
+const age = 15;
+if (age >= 16) {
+    console.log("You are old enough to drive.");
+} else {
+    console.log("You are not old enough to drive.");
+}
+/* ----------- End of Challenge 1 -----------*/
+
+
+/* --------------- Challenge 2 --------------*/
+
+const drinkSize = "small";
+
+switch (drinkSize) {
+    case "small":
+        console.log("A small drink is 10 ounces.");
+        break;
+    case "medium":
+        console.log("A medium drink is 16 ounces.");
+        break;
+    case "large":
+        console.log("A large drink is 22 ounces.");
+        break;
+    default:
+        console.log("Unknown drink size.");
+        break;
+}
